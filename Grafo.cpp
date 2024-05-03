fd4f3489b9bd2c0a0cfd59268911f2530bfe5d90
Rafael Freesz <rafaelfreesz@gmail.com>
2024-05-03 13:52:41 -0300

    Criação da classe Grafo

---
+++ Grafo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rafael on 03/05/2024.
+//
+
+#include "Grafo.h"
---
+++ Grafo.h
@@ -0,0 +1,16 @@
+//
+// Created by Rafael on 03/05/2024.
+//
+
+#ifndef GRAFO_H
+#define GRAFO_H
+
+
+
+class Grafo {
+
+};
+
+
+
+#endif //GRAFO_H
